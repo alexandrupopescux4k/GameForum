9f8c009537225207d690b6f96780068fda114441
Alex <popgabialex@gmail.com>
2025-05-16 12:35:47 +0300

    Finish profile

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -36,7 +36,8 @@ namespace GameForum.Areas.Identity.Pages.Account
             {
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToAction("Index", "Home");
+
             }
         }
     }
